148503bc3140975f1d9007d6b1bee8041b2c789d
andrixxxx <48215354+andrixxxx@users.noreply.github.com>
2019-07-12 23:44:48 +0800

    Update install-premiumscript.sh

--- install-premiumscript.sh
+++ install-premiumscript.sh
@@ -21,7 +21,7 @@ cat > /root/limit.sh <<END3
 END3
 
 cd /usr/local/bin
-wget -O premium-script.tar.gz "https://raw.githubusercontent.com/radzvpn/radzvpn443/master/premium-script.tar.gz"
+wget -O premium-script.tar.gz "https://raw.githubusercontent.com/andrixxxx/443_edited/master/premium-script.tar.gz"
 tar -xvf premium-script.tar.gz
 rm -f premium-script.tar.gz
 
