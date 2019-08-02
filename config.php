335f8efb69d12a318be7fc804d96f4497cd16fa2
andrixxxx <48215354+andrixxxx@users.noreply.github.com>
2019-08-03 04:02:18 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     //
     // vnStat PHP frontend (c)2006-2010 Bjorge Dijkstra (bjd@jooz.net)
     //
-    // This program is free software; you can redistribute it and/or modify
+    // This program is free software; you can redistribute it and/or modify_
     // it under the terms of the GNU General Public License as published by
     // the Free Software Foundation; either version 2 of the License, or
     // (at your option) any later version.
